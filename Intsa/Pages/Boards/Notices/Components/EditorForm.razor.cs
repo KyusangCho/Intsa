276a45f0a5c528aecea99f27f40182eec749f0a3
bryan cho <bryancho31@gmail.com>
2021-02-03 23:33:15 -0600

    Manage 페이지와 EditorForm 컴포넌트 작성

--- Intsa/Pages/Boards/Notices/Components/EditorForm.razor.cs
+++ Intsa/Pages/Boards/Notices/Components/EditorForm.razor.cs
@@ -1,16 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Intsa.Models.Boards;
+using Microsoft.AspNetCore.Components;
+using System;
 
 namespace Intsa.Pages.Boards.Notices.Components
 {
     public partial class EditorForm
     {
 
-        protected override void OnInitialized()
-        {
+        private bool IsShow { get; set; } = false; // 팝업창 표시여부 
+        
+        private string parentId = "0";
 
+        protected int[] parentIds = { 1, 2, 3 };
+
+        public void Show()
+        {
+            IsShow = true; 
+        }
+
+        public void Hide()
+        {
+            IsShow = false; 
+        }
+
+        /// <summary>
+        /// 폼의 제목 영역
+        /// </summary>
+        [Parameter]
+        public RenderFragment EditorFormTitle { get; set; }
+
+        /// <summary>
+        /// 넘어온 모델 개체 
+        /// </summary>
+        [Parameter]
+        public BoardNotices Model { get; set; }
+
+        /// <summary>
+        /// 부모 컴포넌트에게 생성 완료 되었음을 부모 컴포넌트에게 알림 
+        /// </summary>
+        [Parameter]
+        public Action CreateCallback { get; set; }
+
+        /// <summary>
+        /// 부모 컴포넌트에게 수정 완료 되었음을 부모 컴포넌트에게 알림 
+        /// </summary>
+        [Parameter]
+        public EventCallback<bool> EditCallback { get; set; }
+        
+        /// <summary>
+        /// 리포지토리 클래스 참조 
+        /// </summary>
+        [Inject]
+        public INoticeRepositoryAsync NoticeRepositoryAsyncReference { get; set; }
+        
+        protected override void OnParametersSet()
+        {
+            parentId = Model.ParentId.ToString();
+            if (parentId == "0")
+            {
+                parentId = ""; 
+            }
+        }
+
+        protected async void CreateOrEditClick()
+        {
+            if (int.TryParse(parentId, out int newParentId))
+            {
+                newParentId = 0; 
+            }
+            Model.ParentId = newParentId; 
+
+            if (Model.Id == 0)
+            {
+                // Create
+                await NoticeRepositoryAsyncReference.AddAsync(Model);
+                CreateCallback?.Invoke();  // -> EditCallback
+            }
+            else
+            {
+                // Edit 
+                await NoticeRepositoryAsyncReference.EditAsync(Model);
+                await EditCallback.InvokeAsync(true); 
+            }
+            //IsShow = false; 
         }
     }
 }
--- Intsa/Pages/Boards/Notices/Manage.razor.cs
+++ Intsa/Pages/Boards/Notices/Manage.razor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Intsa.Models.Boards;
+using Intsa.Pages.Boards.Notices.Components;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +10,74 @@ namespace Intsa.Pages.Boards.Notices
 {
     public partial class Manage
     {
-        protected override void OnInitialized()
-        {
+        [Inject]
+        public INoticeRepositoryAsync NoticeRepositoryAsyncReference { get; set; }
+        [Inject]
+        public NavigationManager NavigationManagerReference { get; set; }
 
+        public EditorForm EditorFormReference { get; set; }
+
+        protected List<BoardNotices> models;
+        protected BoardNotices model = new BoardNotices(); 
+
+        protected BeanyPager.BeanyPagerBase pager = new BeanyPager.BeanyPagerBase()
+        {
+            PageNumber = 1,
+            PageIndex = 0,
+            PageSize = 10,
+            PagerButtonCount = 5,
+        };
+
+        protected override async Task OnInitializedAsync()
+        {
+            await DisplayData();
+
+        }
+
+        private async Task DisplayData()
+        {
+            //await Task.Delay(3000); 
+            var resultSet = await NoticeRepositoryAsyncReference.GetAllAsync(pager.PageIndex, pager.PageSize);
+            pager.RecordCount = resultSet.TotalRecords;
+            models = resultSet.Records.ToList();
+        }
+
+        protected void NameClick(int id)
+        {
+            NavigationManagerReference.NavigateTo($"/Boards/Notices/Details/{id}");
+        }
+
+        protected async void PageIndexChanged(int pageIndex)
+        {
+            pager.PageIndex = pageIndex;
+            pager.PageNumber = pageIndex + 1;
+
+            await DisplayData();
+
+            StateHasChanged();
+        }
+
+        public string EditorFormTitle { get; set; } = "CREATE"; 
+
+        protected void ShowEditorForm()
+        {
+            EditorFormTitle = "CREATE";
+            this.model = new BoardNotices(); 
+            EditorFormReference.Show(); 
+        }
+
+        protected void EditBy(BoardNotices model)
+        {
+            EditorFormTitle = "EDIT";
+            this.model = model; 
+            EditorFormReference.Show(); 
+        }
+
+        protected async void CreateOrEdit()
+        {
+            EditorFormReference.Hide();
+            await DisplayData();
+            StateHasChanged(); 
         }
     }
 }
