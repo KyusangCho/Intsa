2eb5f3808baf3755d82c872962b73bcbf454b95f
bryan cho <bryancho31@gmail.com>
2021-02-18 07:06:06 -0600

    로그인 변경

---
+++ Intsa/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intsa.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string email, string password)
+        {
+
+
+
+            return View(); 
+        }
+    }
+}
