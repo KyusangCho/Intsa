1f944c9255c96374b419e5f832dbfa016361eb48
bryan cho <bryancho31@gmail.com>
2021-02-26 23:30:18 -0600

    Syncfusion Nuget 정리, 게시판 RichTextEditor 오류 > 큰 테이블 저장못하는 문제 발생

--- Intsa/Controllers/FileUploadController.cs
+++ Intsa/Controllers/FileUploadController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net.Http.Headers;
 
 namespace Intsa.Controllers
 {
--- Intsa/Pages/Boards/Notices/Create.razor.cs
+++ Intsa/Pages/Boards/Notices/Create.razor.cs
@@ -1,12 +1,10 @@
 ﻿using Intsa.Models.Boards;
-using Intsa.Shared.Utils;
 using Microsoft.AspNetCore.Components;
 using Syncfusion.Blazor.RichTextEditor;
-using Syncfusion.Blazor.Inputs; 
+using Syncfusion.Blazor.Inputs;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.IO;
-using Microsoft.AspNetCore.Http;
 
 namespace Intsa.Pages.Boards.Notices
 {
@@ -43,16 +41,16 @@ namespace Intsa.Pages.Boards.Notices
             NavigationManagerReference.NavigateTo("/Boards/Notices"); 
         }
 
-        private void onRemove(RemovingEventArgs args)
-        {
-            foreach (var removeFile in args.FilesData)
-            {
-                if (File.Exists(Path.Combine(@"rootPath", removeFile.Name)))
-                {
-                    File.Delete(Path.Combine(@"rootPath", removeFile.Name)); 
-                }
-            }
-        }
+        //private void onRemove(RemovingEventArgs args)
+        //{
+        //    foreach (var removeFile in args.FilesData)
+        //    {
+        //        if (File.Exists(Path.Combine(@"rootPath", removeFile.Name)))
+        //        {
+        //            File.Delete(Path.Combine(@"rootPath", removeFile.Name)); 
+        //        }
+        //    }
+        //}
 
         private List<ToolbarItemModel> Tools = new List<ToolbarItemModel>()
         {
@@ -63,31 +61,51 @@ namespace Intsa.Pages.Boards.Notices
             new ToolbarItemModel() { Command = ToolbarCommand.FontName },
             new ToolbarItemModel() { Command = ToolbarCommand.FontSize },
             new ToolbarItemModel() { Command = ToolbarCommand.FontColor },
-            new ToolbarItemModel() { Command = ToolbarCommand.BackgroundColor },
-            new ToolbarItemModel() { Command = ToolbarCommand.LowerCase },
-            new ToolbarItemModel() { Command = ToolbarCommand.UpperCase },
-            new ToolbarItemModel() { Command = ToolbarCommand.SuperScript },
-            new ToolbarItemModel() { Command = ToolbarCommand.SubScript },
+            //new ToolbarItemModel() { Command = ToolbarCommand.BackgroundColor },
+            //new ToolbarItemModel() { Command = ToolbarCommand.LowerCase },
+            //new ToolbarItemModel() { Command = ToolbarCommand.UpperCase },
+            //new ToolbarItemModel() { Command = ToolbarCommand.SuperScript },
+            //new ToolbarItemModel() { Command = ToolbarCommand.SubScript },
             new ToolbarItemModel() { Command = ToolbarCommand.Separator },
-            new ToolbarItemModel() { Command = ToolbarCommand.Formats },
+            //new ToolbarItemModel() { Command = ToolbarCommand.Formats },
             new ToolbarItemModel() { Command = ToolbarCommand.Alignments },
             new ToolbarItemModel() { Command = ToolbarCommand.OrderedList },
             new ToolbarItemModel() { Command = ToolbarCommand.UnorderedList },
             new ToolbarItemModel() { Command = ToolbarCommand.Outdent },
             new ToolbarItemModel() { Command = ToolbarCommand.Indent },
-            new ToolbarItemModel() { Command = ToolbarCommand.Separator },
-            new ToolbarItemModel() { Command = ToolbarCommand.CreateLink },
+            //new ToolbarItemModel() { Command = ToolbarCommand.Separator },
+            //new ToolbarItemModel() { Command = ToolbarCommand.CreateLink },
             new ToolbarItemModel() { Command = ToolbarCommand.Image },
             new ToolbarItemModel() { Command = ToolbarCommand.CreateTable },
-            new ToolbarItemModel() { Command = ToolbarCommand.Separator },
-            new ToolbarItemModel() { Command = ToolbarCommand.ClearFormat },
-            new ToolbarItemModel() { Command = ToolbarCommand.Print },
-            new ToolbarItemModel() { Command = ToolbarCommand.SourceCode },
-            new ToolbarItemModel() { Command = ToolbarCommand.Separator },
+            //new ToolbarItemModel() { Command = ToolbarCommand.Separator },
+            //new ToolbarItemModel() { Command = ToolbarCommand.ClearFormat },
+            //new ToolbarItemModel() { Command = ToolbarCommand.Print },
+            //new ToolbarItemModel() { Command = ToolbarCommand.SourceCode },
+            //new ToolbarItemModel() { Command = ToolbarCommand.Separator },
             new ToolbarItemModel() { Command = ToolbarCommand.Undo },
             new ToolbarItemModel() { Command = ToolbarCommand.Redo }
         };
-        
-        
+
+        private List<ImageToolbarItemModel> Image = new List<ImageToolbarItemModel>()
+    {
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.Replace },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.Align },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.Caption },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.Remove },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.OpenImageLink },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.HorizontalSeparator },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.EditImageLink },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.RemoveImageLink },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.Display },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.AltText },
+        new ImageToolbarItemModel() { Command = ImageToolbarCommand.Dimension }
+    };
+
+        private List<LinkToolbarItemModel> Link = new List<LinkToolbarItemModel>()
+    {
+        new LinkToolbarItemModel() { Command = LinkToolbarCommand.Open },
+        new LinkToolbarItemModel() { Command = LinkToolbarCommand.Edit },
+        new LinkToolbarItemModel() { Command = LinkToolbarCommand.UnLink }
+    };
     }
 }
