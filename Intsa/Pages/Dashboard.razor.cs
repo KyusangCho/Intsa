f120cd8282dff6b4fd8adb4c0600e8c901091a04
bryan cho <bryancho31@gmail.com>
2021-05-11 06:33:24 -0600

    Dashboard Chart 추가

--- Intsa/Pages/Dashboard.razor.cs
+++ Intsa/Pages/Dashboard.razor.cs
@@ -5,7 +5,7 @@ using Intsa.Pages.Components;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System;
 
 namespace Intsa.Pages
 {
@@ -106,5 +106,28 @@ namespace Intsa.Pages
 
             StateHasChanged();
         }
+
+        public class ChartData
+        {
+            public DateTime XValue { get; set; }
+            public double YValue { get; set; }
+        }
+
+        public List<ChartData> Data = new List<ChartData>
+{
+        new ChartData { XValue = new DateTime(2000, 2, 11), YValue = 14 },
+        new ChartData { XValue = new DateTime(2001, 9, 4), YValue = 20 },
+        new ChartData { XValue = new DateTime(2002, 2, 11), YValue = 25 },
+        new ChartData { XValue = new DateTime(2003, 9, 16), YValue = 21 },
+        new ChartData { XValue = new DateTime(2004, 2, 7), YValue = 13},
+        new ChartData { XValue = new DateTime(2005, 9, 7), YValue = 18 },
+        new ChartData { XValue = new DateTime(2006, 2, 11), YValue = 24 },
+        new ChartData { XValue = new DateTime(2007, 9, 14), YValue = 23 },
+        new ChartData { XValue = new DateTime(2008, 2, 6), YValue = 19 },
+        new ChartData { XValue = new DateTime(2009, 9, 6), YValue = 31 },
+        new ChartData { XValue = new DateTime(2010, 2, 11), YValue = 39},
+        new ChartData { XValue = new DateTime(2011, 9, 11), YValue = 50 },
+        new ChartData { XValue = new DateTime(2012, 2, 11), YValue = 24 },
+    };
     }
 }
